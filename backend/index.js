97423072a4b1c65ca2187e949532b938d09f17d3
Ali <AliNazarii7394@gmail.com>
2023-09-30 10:27:11 +0330

    create a Route for saving new books with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,42 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+//Middleware for parcing request body
+app.use(express.json());
+
+
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+//Route
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res
+        .status(400)
+        .send({ message: "All field Required ( title , author , publishYear )" });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    res.status(201).send({ book });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: error });
+  }
+});
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("WElCOM to my Project (ALI NAZARi)  ");
+});
 
 mongoose
   .connect(process.env.MONGO)
@@ -17,8 +49,3 @@ mongoose
   .catch((error) => {
     console.log(console.log(error));
   });
-
-app.get("/", (req, res) => {
-  console.log(req);
-  return res.status(234).send("WElCOM to my Project (ALI NAZARi)  ");
-});
